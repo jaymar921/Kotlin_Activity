e922e221bdadf5670856e03ce3e8a3df06e58be3
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-02-16 09:31:16 +0800

    Add files via upload

---
+++ Activities/StringToInteger.kt
@@ -0,0 +1,16 @@
+/*
+    Created by Jayharron @JayMar on 16/2/2022
+ */
+
+fun main(args : Array<String>){
+
+    val str : String = "4"
+
+    var java : Int = Integer.parseInt(str) // using the Java way
+    var kotlin : Int = str.toInt()         // using the Kotlin way
+
+    java++ // increment java
+    kotlin+2 // increment kotlin by 2
+    println("Java = $java")
+    println("Kotlin = $kotlin")
+}
