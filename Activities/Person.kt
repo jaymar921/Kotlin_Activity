7aa54b7ffd0700e1da55ebe7299655916d7158fb
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-02-16 10:32:08 +0800

    Add files via upload

---
+++ Activities/Person.kt
@@ -0,0 +1,12 @@
+/*
+    Created by Jayharron @JayMar on 16/2/2022
+
+    Required @ExtensionFunction.kt
+ */
+class Person {
+    var skills : String? = null
+
+    fun show(){
+        println(skills)
+    }
+}
