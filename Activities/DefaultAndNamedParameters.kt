4ade6cb675d57677286ca66f59b972f60c4f56c2
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-02-16 09:23:53 +0800

    Add files via upload

---
+++ Activities/DefaultAndNamedParameters.kt
@@ -0,0 +1,17 @@
+
+fun main(args : Array<String>){
+
+    var finalAmt = calcAmount(50) // using the default parameter
+    println(finalAmt)
+
+    finalAmt = calcAmount(amount = 100, interest = 0.03) // using named parameter
+    println(finalAmt)
+
+    finalAmt = calcAmount(interest = 0.03, amount = 100) // it can be interchanged
+    println(finalAmt)
+}
+
+@JvmOverloads // Optional, it will create 2 methods for Java to use default and named @params
+fun calcAmount(amount : Int, interest : Double = 0.04) : Int{
+    return (amount + amount * interest).toInt()
+}
